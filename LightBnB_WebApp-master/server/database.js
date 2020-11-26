cbe977ab8db4e3b96b88f47a16def6d43725a87e
RAFH82 <hrechkar@gmail.com>
2020-11-26 21:09:09 +0000

    Updated getAllReservations with SQL query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -57,7 +57,6 @@ const addUser = function (user) {
 		if (res.rows.length === 0) {
 			return null;
 		}
-		console.log(res.rows[0]);
 		return res.rows[0];
 	});
 };
@@ -71,7 +70,19 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-	return getAllProperties(null, 2);
+	// return getAllProperties(null, 2);
+	const query = `SELECT properties.*, reservations.*,
+  AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`;
+	const values = [guest_id, limit];
+	return pool.query(query, values).then((res) => res.rows);
 };
 exports.getAllReservations = getAllReservations;
 
